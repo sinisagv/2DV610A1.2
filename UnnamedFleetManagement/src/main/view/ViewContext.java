43a63b03a754641ba91d1d5a64befd2faa9a3da5
Dustin Payne <dp222gr@student.lnu.se>
2019-11-25 14:55:48 +0100

    RED added should_display_new_view_on_input() on ViewContextTest

--- UnnamedFleetManagement/src/main/view/ViewContext.java
+++ UnnamedFleetManagement/src/main/view/ViewContext.java
@@ -1,15 +1,20 @@
 package main.view;
 
+import java.util.Scanner;
+
 public class ViewContext {
 
 	private IView view;
 
-	public ViewContext(IView primaryView) {
+	public ViewContext(IView primaryView, Scanner input) {
 		this.view = primaryView;
 	}
 
 	public void start() {
 		view.show();
 	}
-
+	
+	public void switchView(IView newView) {
+		
+	}
 }
--- UnnamedFleetManagement/src/test/view/ViewContextTest.java
+++ UnnamedFleetManagement/src/test/view/ViewContextTest.java
@@ -3,6 +3,12 @@ package test.view;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +20,24 @@ class ViewContextTest {
 	@Test
 	void start_should_show_first_view() {
 		IView mockedIView = mock(IView.class);
-		ViewContext SUT = new ViewContext(mockedIView);
+		ViewContext SUT = new ViewContext(mockedIView, null);
 		SUT.start();
 		verify(mockedIView, atLeastOnce()).show();
+	}
+	
+	@Test
+	void should_change_views_on_input() {
+		IView mockedIView = mock(IView.class);
+		IView testView = mock(IView.class);
+		
+		Scanner sc = new Scanner("1"); // scanner that returns 1
+		
+		ViewContext SUT = new ViewContext(mockedIView, sc);
+		mockedIView.addOption("testOption", () -> {
+			SUT.switchView(testView);
+		});
+		verify(testView, times(1)).show(); // checks that show() is called on the first option of menu 1
+		
 	}
 
 }
