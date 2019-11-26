a1a27074ba56db1c40d79fc99861c880f31c556f
Sinisa <gvojics@gmail.com>
2019-11-26 13:43:26 +0100

    REFACTOR added getClient() to Booking

--- UnnamedFleetManagement/src/main/model/Booking.java
+++ UnnamedFleetManagement/src/main/model/Booking.java
@@ -7,35 +7,46 @@ public class Booking {
 	private TimeFrame timeFrame;
 	private ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
 	private ArrayList<Driver> drivers;
-	
+
 	public ArrayList<Driver> getDrivers() {
 		return drivers;
 	}
+
 	public boolean addVehicle(Vehicle vehicle) {
-		for(Vehicle v : vehicles) {
-			if(v.equals(vehicle)) {
+		for (Vehicle v : vehicles) {
+			if (v.equals(vehicle)) {
 				return false;
 			}
 		}
 		return vehicles.add(vehicle);
 	}
+
 	public boolean removeVehicle(Vehicle vehicle) {
 		return vehicles.remove(vehicle);
 	}
+
 	public boolean addDriver(Driver driver) {
 		return false;
 	}
+
 	public boolean removeDriver(Driver driver) {
 		return false;
 	}
+
 	public void setClient(Client client) {
 		this.client = client;
 	}
+
+	public Client getClient() {
+		return this.client;
+	}
+
 	public boolean removeClient(Client client) {
 		return false;
 	}
+
 	public ArrayList<Vehicle> getVehicles() {
 		return vehicles;
 	}
-	
+
 }
