9f78ded41d4af9bd3239c9ed968e901aaa2470db
Dustin Payne <dp222gr@student.lnu.se>
2019-11-26 19:11:27 +0100

    RED added should_show_list_of_items() to ListMenuViewTest.java

--- UnnamedFleetManagement/src/test/view/ListMenuViewTest.java
+++ UnnamedFleetManagement/src/test/view/ListMenuViewTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,8 +22,30 @@ class ListMenuViewTest {
 		OutputStream out = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(out));
 		SUT.show();
-		String actual =  out.toString().split("\n")[0];
+		String actual = out.toString().split("\n")[0];
 		assertEquals(testString, actual);
+	}
+
+	@Test
+	void should_show_list_of_items() {
+		String expectedListContent = "ListContent";
+		List<String> expectedList = new ArrayList<String>();
+
+		for (int i = 0; i < 100; i++) {
+			expectedList.add(expectedListContent);
+		}
+
+		IView SUT = new ListMenuView("TestMenu", expectedList);
+
+		OutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		SUT.show();
+
+		for (int i = 1; i < 101; i++) {
+			String[] actual = out.toString().split("\n");
+			assertEquals(expectedList.get(i), actual[i]);
+		}
+
 	}
 
 }
