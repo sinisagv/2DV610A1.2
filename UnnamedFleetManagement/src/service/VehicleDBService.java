f8c550bc87b8c0d45084016dc5a01495e6641ae7
Sinisa <gvojics@gmail.com>
2019-11-27 19:04:20 +0100

    GREEN added a correct implementation for the write() method and added constructor + storage files

--- UnnamedFleetManagement/src/service/VehicleDBService.java
+++ UnnamedFleetManagement/src/service/VehicleDBService.java
@@ -1,20 +1,55 @@
 package service;
 
+import java.io.File;
 import java.util.UUID;
 
-import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import main.model.CargoType;
 import main.model.Fleet;
 import main.model.Vehicle;
 
 public class VehicleDBService implements IDBService {
 
-	private String dbPath = "";
+	private String dbPath = "src/service/vehicles.xml";
 	private Document dataDoc;
 	private Fleet vehicles = new Fleet();
 
-	public VehicleDBService() throws Exception {
+	public VehicleDBService(String filepath) throws Exception {
 
+		File data = new File(filepath);
+		DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = dbfactory.newDocumentBuilder();
+		dataDoc = documentBuilder.parse(data);
+
+		NodeList vehicleNodes = dataDoc.getElementsByTagName("vehicle");
+
+		for (int i = 0; i < vehicleNodes.getLength(); i++) {
+			String tempFName = dataDoc.getElementsByTagName("capacity").item(i).getTextContent();
+			String tempLName = dataDoc.getElementsByTagName("volume").item(i).getTextContent();
+			String tempID = dataDoc.getElementsByTagName("ID").item(i).getTextContent();
+			String tempPersonNo = dataDoc.getElementsByTagName("cargo").item(i).getTextContent();
+			Vehicle tempVehicle = new Vehicle();
+
+			tempVehicle.setCapacity(Integer.parseInt(tempFName));
+			tempVehicle.setVolume(Integer.parseInt(tempLName));
+			tempVehicle.setID(tempID);
+			tempVehicle.setCargoType(Enum.valueOf(CargoType.class, tempPersonNo));
+
+			vehicles.addVehicle(tempVehicle);
+
+		}
 	}
 
 	@Override
@@ -25,31 +60,95 @@ public class VehicleDBService implements IDBService {
 
 	@Override
 	public boolean write() {
-		// TODO Auto-generated method stub
-		return false;
+		DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+
+		try {
+			DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+
+			Document document = documentBuilder.newDocument();
+
+			// root element
+			Element root = document.createElement("root");
+			document.appendChild(root);
+
+			// list of members
+			Element vehicleNodes = document.createElement("vehicles");
+
+			root.appendChild(vehicleNodes);
+
+			// create members
+
+			for (Vehicle v : vehicles) {
+
+				// create member
+
+				Element vehicleElement = document.createElement("vehicle");
+
+				// add fName
+				Element capacity = document.createElement("capacity");
+				capacity.appendChild(document.createTextNode(Integer.toString(v.getCapacity())));
+				vehicleElement.appendChild(capacity);
+
+				// add lName
+				Element volume = document.createElement("volume");
+				volume.appendChild(document.createTextNode(Integer.toString(v.getVolume())));
+				vehicleElement.appendChild(volume);
+
+				// add memberID
+				Element ID = document.createElement("ID");
+				ID.appendChild(document.createTextNode(v.getID()));
+				vehicleElement.appendChild(ID);
+
+				// add phoneNo
+				Element cargo = document.createElement("cargo");
+				cargo.appendChild(document.createTextNode(v.getCargoType().toString()));
+				vehicleElement.appendChild(cargo);
+
+				// add member to memberlist
+				vehicleNodes.appendChild(vehicleElement);
+			}
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer;
+			try {
+				transformer = transformerFactory.newTransformer();
+
+				DOMSource domSource = new DOMSource(document);
+				StreamResult streamResult = new StreamResult(new File(dbPath));
+
+				// If you use
+				// StreamResult result = new StreamResult(System.out);
+				// the output will be pushed to the standard output ...
+				// You can use that for debugging
+
+				transformer.transform(domSource, streamResult);
+			} catch (TransformerException e) {
+				return false;
+			}
+		} catch (ParserConfigurationException e) {
+			return false;
+		}
+		return true;
+
 	}
-	
+
 	public boolean addVehicle(Vehicle vehicle) {
 		if (vehicle.getID() == null || vehicle.getID() == "") {
 			vehicle.setID(generateID());
 		}
-		
+
 		return vehicles.addVehicle(vehicle) && write();
 	}
-	
+
 	private String generateID() {
-		// TODO Auto-generated method stub
 		return UUID.randomUUID().toString();
 	}
 
 	public boolean removeVehicle(Vehicle vehicle) {
 		return false;
 	}
-	
+
 	public Fleet getFleet() {
 		return this.vehicles;
 	}
-	
-	
 
 }
--- UnnamedFleetManagement/src/test/service/VehicleDBServiceTest.java
+++ UnnamedFleetManagement/src/test/service/VehicleDBServiceTest.java
@@ -3,10 +3,17 @@ package test.service;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import main.model.CargoType;
 import main.model.Vehicle;
 import service.VehicleDBService;
 
@@ -14,6 +21,11 @@ class VehicleDBServiceTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		BufferedWriter writer = Files.newBufferedWriter(Paths.get("src/test/service/testFile.xml"));
+		writer.write("");
+		writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" + 
+				"<root></root>");
+		writer.flush();
 	}
 
 	@AfterEach
@@ -22,15 +34,32 @@ class VehicleDBServiceTest {
 
 	@Test
 	void addVehicleShouldReturnTrue() throws Exception {
-		VehicleDBService sut = new VehicleDBService();
-		assertTrue(sut.addVehicle(new Vehicle()));
-		//assertEquals(sut.getFleet().size(), 1);
+		VehicleDBService sut = new VehicleDBService("src/test/service/testFile.xml");
+		Vehicle v = new Vehicle();
+		v.setCapacity(10);
+		v.setCargoType(CargoType.PASSENGERS);
+		v.setVolume(20);
+		assertTrue(sut.addVehicle(v));
+		assertEquals(sut.getFleet().size(), 1);
 	}
 	
 	@Test
 	void writeShouldReturnTrue() throws Exception {
-		VehicleDBService sut = new VehicleDBService();
+		VehicleDBService sut = new VehicleDBService("src/test/service/testFile.xml");
+		Vehicle v = new Vehicle();
+		File db = new File("src/test/service/testFile.xml");
+		Scanner sc = new Scanner(db);
+		int lines = 0;
+		while(sc.hasNextLine()) {
+			sc.nextLine();
+			lines++;
+		}
+		v.setCapacity(10);
+		v.setCargoType(CargoType.PASSENGERS);
+		v.setVolume(20);
 		assertTrue(sut.write());
+		assertTrue(lines >= 1);
+		sc.close();
 	}
 
 }
