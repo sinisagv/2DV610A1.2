ffa5bb790df6ddaff49b9c548fcfacedc3d91d43
Dustin Payne <dp222gr@student.lnu.se>
2019-11-28 20:04:40 +0100

    RED added launch_IOExceptionFromDBFactory_shouldAlertUser() in AppMainTest

---
+++ UnnamedFleetManagement/src/main/AppMain.java
@@ -0,0 +1,16 @@
+package main;
+
+import main.service.DBServiceFactory;
+import main.view.ViewContext;
+
+public class AppMain {
+
+	public AppMain(ViewContext viewController, DBServiceFactory databaseFactory) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void launch() {
+		
+	}
+
+}
--- UnnamedFleetManagement/src/test/controller/AppMainTest.java
+++ UnnamedFleetManagement/src/test/controller/AppMainTest.java
@@ -1,24 +1,49 @@
 package test.controller;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
 
-import main.service.VehicleDBService;
+import main.AppMain;
+import main.service.DBServiceFactory;
 import main.view.ViewContext;
 
 class AppMainTest {
 
 	@Test
-	void launch_badDBFilePath_shouldPropmtandQuit() {
-		VehicleDBService mockServ = mock(VehicleDBService.class);
-		DBServiceFactory mockServFac = mock(VehicleDBServiceFactory.class);
-		when(mockServFac.getVehicleDBService(WRONG_FILE_PATH)).thenThrow(IOException.class);
+	void launch_IOExceptionFromDBFactory_shouldAlertUser() {
+		// set up output stream that we can monitor
+		ByteArrayOutputStream testByteStream = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(testByteStream));
+
+		// set up mock DBServiceFactory
+		DBServiceFactory mockDBFac = mock(DBServiceFactory.class);
+		try {
+			doThrow(new IOException()).when(mockDBFac).getVehicleDBService(any());
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		AppMain SUT = new AppMain(mock(ViewContext.class), mockServ);
+		// set up SUT
+		AppMain SUT = new AppMain(mock(ViewContext.class), mockDBFac);
+		SUT.launch();
 		
+		String expected = "Unable to connect to database. Please contact your SysAdmin";
+		
+		// get actual
+		String actual = testByteStream.toString();
+		
+		assertEquals(expected, actual);
+
 	}
-	
+
 }
