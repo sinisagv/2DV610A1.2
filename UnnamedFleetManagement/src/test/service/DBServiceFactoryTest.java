5f1b2ff407af3c11845f298914595eef8ec40050
Sinisa <gvojics@gmail.com>
2019-11-28 19:07:40 +0100

    GREEN method implemented

--- UnnamedFleetManagement/src/test/service/DBServiceFactoryTest.java
+++ UnnamedFleetManagement/src/test/service/DBServiceFactoryTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
 
 import main.service.DBServiceFactory;
 
@@ -23,7 +26,18 @@ class DBServiceFactoryTest {
 	@Test
 	void getVehicleDBServiceShouldReturnNotNull() {
 		DBServiceFactory sut = new DBServiceFactory();
-		assertNotNull(sut.getVehicleDBService("src/test/service/testFile.xml"));
+		try {
+			assertNotNull(sut.getVehicleDBService("src/test/service/testFile.xml"));
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		assertThrows(IOException.class, () -> {
 			sut.getVehicleDBService("fsef352/532");
 		});
