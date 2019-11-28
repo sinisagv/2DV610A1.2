442fbe1ea4fad2426b0751f119cf4b22e4c83e31
Dustin Payne <dp222gr@student.lnu.se>
2019-11-28 19:48:52 +0100

    Revert "Added AppMainTest and test: launch_badDBFilePath_shouldThrowIOException" This test was stupid. I shoulde not have added it This reverts commit 5a25bee1d3398e1288813c7eb40b6a7c19b88dd2.

--- UnnamedFleetManagement/src/test/controller/AppMainTest.java
+++ UnnamedFleetManagement/src/test/controller/AppMainTest.java
@@ -1,31 +1,23 @@
 package test.controller;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
-import main.controller.AppMain;
-import main.service.DBServiceFactory;
+import main.service.VehicleDBService;
 import main.view.ViewContext;
 
 class AppMainTest {
 
 	@Test
-	void launch_badDBFilePath_shouldThrowIOException() {
-		DBServiceFactory mockServFac = mock(DBServiceFactory.class);
-		try {
-			when(mockServFac.getVehicleDBService(any())).thenThrow(IOException.class);
-		} catch (Exception e) {
-			
-		} 
+	void launch_badDBFilePath_shouldPropmtandQuit() {
+		VehicleDBService mockServ = mock(VehicleDBService.class);
+		DBServiceFactory mockServFac = mock(VehicleDBServiceFactory.class);
+		when(mockServFac.getVehicleDBService(WRONG_FILE_PATH)).thenThrow(IOException.class);
 		
-		AppMain SUT = new AppMain(mock(ViewContext.class), mockServFac);
-		assertThrows(IOException.class, () -> SUT.launch());
+		AppMain SUT = new AppMain(mock(ViewContext.class), mockServ);
 		
 	}
 	
