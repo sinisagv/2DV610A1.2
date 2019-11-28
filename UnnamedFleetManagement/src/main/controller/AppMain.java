89418e74c630c2f20646840239b3eabc56ef0c4c
Dustin Payne <dp222gr@student.lnu.se>
2019-11-28 21:09:39 +0100

    REFACTOR added some submenus to manually test integration and use the VehicleDBService

--- UnnamedFleetManagement/src/main/controller/AppMain.java
+++ UnnamedFleetManagement/src/main/controller/AppMain.java
@@ -12,35 +12,45 @@ import main.service.VehicleDBService;
 import main.view.MenuView;
 
 public class AppMain {
-	
+
 	private ViewController viewController;
 	private DBServiceFactory databaseFactory;
 	private final String VEHICLE_DB_FILE_PATH = "src/main/service/vehicles.xml";
-	
-	
+
 	public AppMain(ViewController viewController, DBServiceFactory databaseFactory) {
 		this.viewController = viewController;
 		this.databaseFactory = databaseFactory;
 	}
-	
+
 	public void launch() {
-		VehicleDBService vehicleDatabase;
+		VehicleDBService vehicleDatabase = null;
 		try {
 			vehicleDatabase = databaseFactory.getVehicleDBService(VEHICLE_DB_FILE_PATH);
-		} catch(IOException | ParserConfigurationException | SAXException e) {
+		} catch (IOException | ParserConfigurationException | SAXException e) {
 			System.err.print("Unable to connect to database. Please contact your SysAdmin");
-			
+		}
+		if (vehicleDatabase == null) {
+			System.exit(0);
 		}
 		MenuView mainMenu = new MenuView("Welcome to Unnamed Fleet Management Tool (name TBD)");
+		MenuView fleetSummaryView = new MenuView(
+				"Fleet Summary: " + vehicleDatabase.getFleet().size() + " vehicles registered.");
+		fleetSummaryView.addOption("Back to main menu", () -> {
+			viewController.showView(mainMenu).run();
+		});
+
+		mainMenu.addOption("Display Fleet Summary", () -> {
+			viewController.showView(fleetSummaryView).run();
+		});
 		mainMenu.addOption("Quit", () -> System.exit(1));
-		viewController.showView(mainMenu);
+		viewController.showView(mainMenu).run();
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		AppMain app = new AppMain(new ViewController(new Scanner(System.in)), new DBServiceFactory());
 		app.launch();
-		
+
 	}
 
 }
